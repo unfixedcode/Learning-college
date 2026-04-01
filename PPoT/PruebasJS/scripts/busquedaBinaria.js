eb327ea809e91fba7e42bf5bb3573b326ad51dff
Felipe Ayala <unfixed.code@gmail.com>
2026-04-01 15:40:54 -0500

    Added: - EN-ES Translation for footer section in principal index.html (projects portal) with Google Translate. Also added a new section in the footer to credit Google Translate for the translations. - Small corrections to CSS style and general code in: principal index, algorithms page, and the two search algorithm scripts (binary and linear search).

--- PPoT/PruebasJS/scripts/busquedaBinaria.js
+++ PPoT/PruebasJS/scripts/busquedaBinaria.js
@@ -1,27 +1,38 @@
 function busquedaBinariaJS() { // Sirve para "empaquetar" el script y solo se ejecute como función si se llama, no automáticamente al abrir el index
-// Pruebas de scripts JS - Búsqueda binaria //
-const num = [23, 87, 45, 12, 66, 9, 34, 78, 51, 4, 29, 90, 17, 63, 38];
-// Ordenar el array para la búsqueda binaria
-num.sort(function(a, b) {
-    return a - b;
-});
-// Testeo= console.log(num)
-// Función de búsqueda binaria
-let numBuscado = prompt("Ingrese el número a buscar:");
-console.log("Búsqueda Binaria (ciclo mientras o while):");
-}
-
-let [i] = 0;
-
-if ( numBuscado == num[i]) {
-    console.log("Número encontrado: " + numBuscado + " en la posición " + num.indexOf(numBuscado));
-    alert("Número encontrado: '" + numBuscado + "' en la posición '" + num.indexOf(numBuscado) + "' del array o también puedes ver el proceso en consola");
-
-}
-
-// Verificación si está o no en el array el input
-if(!encontrado) {
-    console.log(numBuscado + " no está dentro del array");
+    // Pruebas de scripts JS - Búsqueda binaria //
+    const num = [23, 87, 45, 12, 66, 9, 34, 78, 51, 4, 29, 90, 17, 63, 38];
+    // Ordenar el array para la búsqueda binaria
+    num.sort(function(a, b) {
+        return a - b;
+    });
+    // Testeo= console.log(num)
+    // Prompt y consola
+    let numBuscado = parseInt(prompt("Ingrese el número a buscar:"));
+    console.log("Búsqueda Binaria (ciclo mientras o while):");
+     // Variables para el inicio, fin y estado de encontrado
+    let inicio = 0;
+    let fin = num.length - 1;
+    let encontrado = false;
+    
+    // Ciclo de búsqueda binaria
+    while (inicio <= fin) {
+        let medio = Math.floor((inicio + fin) / 2);
+        if (numBuscado === num[medio]) {
+            encontrado = true;
+            console.log("Número encontrado: '" + numBuscado + "' en la posición " + medio);
+            alert("Número encontrado: '" + numBuscado + "' en la posición '" + medio + "' del array o también puedes ver el proceso en consola");
+            break;
+        } else if (numBuscado < num[medio]) {
+            fin = medio - 1;
+        } else {
+            inicio = medio + 1;
+        }
+    }
+    
+    // Verificación si está o no en el array el input
+    if(!encontrado) {
+        console.log(numBuscado + " no está dentro del array");
+    }
 }
 
 // let i = 0; // variable iteradora iniciando en 0
